8e69809fc9e00065656fe3dfd14d9f14ef4ac10a
bdbenu <bdbenu18@gmail.com>
2024-02-18 08:47:27 +0530

    initial commit

---
+++ test190/src/main/java/com/hospital/Test190Application.java
@@ -0,0 +1,13 @@
+package com.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test190Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test190Application.class, args);
+	}
+
+}
